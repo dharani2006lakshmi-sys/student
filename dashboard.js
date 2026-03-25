efa78fda71e8cb2abdb2698d5078ca755dfae25f
dharani2006lakshmi-sys <dharani2006lakshmi@gmail.com>
2026-03-26 00:04:56 +0530

    Update and rename dashboard.js to complete_profile.html

--- dashboard.js
+++
@@ -1,9 +0,0 @@
-import { db } from "./firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
-async function load() {
-const snap = await getDocs(collection(db, "students"));
-document.getElementById("count").innerText = snap.size;
-}
-
-load();
